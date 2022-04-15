61a912f0c0ec67d2daf0c57b0bb67171eeab7695
caglar.durmus <caglardur@gmail.com>
2022-04-16 00:25:40 +0300

    title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-col class="mx-auto text-center">Vue2 Infinite Scroll Example</v-col>
+      <v-col sm="12" md="6" lg="4" class="d-flex align-center ma-0 pa-0 mx-auto">
+        <v-row align="center">
+          <v-col>Vue2 Infinite Scroll Example </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="auto">
+            <v-btn icon href="https://github.com/caglardur/vue-infinite-scroll" target="_blank"><v-icon>mdi-github</v-icon></v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
